c15644e046b3df13960eb06f9372c9b384bba3e9
Jesper Fan <jesperfanyz@gmail.com>
2024-03-19 16:56:09 +0800

    http secure

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ const sessionConfig = {
     cookie: {
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7,
-        // secure: true,
+        secure: true,
         httpOnly: true
     }
 }
