1ec31edd8e65321d03b28b28ef99435c9b37009b
Jesper Fan <jesperfanyz@gmail.com>
2024-03-19 17:02:44 +0800

    Singapore Center

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -3,7 +3,7 @@
         container: 'cluster-map',
         // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
         style: 'mapbox://styles/mapbox/dark-v11',
-        center: [-103.5917, 40.6699],
+        center: [1.350288, 103.808387], //Singapore Center
         zoom: 3
     });
 
